f4f65d3e7b0a1ed86055088711bbc8bd6dcf5151
Trevor Gunn <gunn317729530@gmail.com>
2020-03-26 21:22:50 +0800

    project init

---
+++ src/main/java/com/gunn/App.java
@@ -0,0 +1,13 @@
+package com.gunn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
